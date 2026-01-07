279f9aa1b5212fcc71cda418b84f929de6cd061b
eddolody-ui <eddolody@gmail.com>
2026-01-07 09:44:12 +0630

    Update axois.ts

--- client/src/api/axois.ts
+++ client/src/api/axois.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  baseURL: "https://retailshopbackend.onrender.com/api",
 
   headers: { "Content-Type": "application/json" },
 });
 export default api;
+
