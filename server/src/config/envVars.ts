77f8259824dcbba9067d713937a6781f16609783
eddolody-ui <eddolody@gmail.com>
2026-01-07 12:39:02 +0630

    Update envVars.ts

--- server/src/config/envVars.ts
+++ server/src/config/envVars.ts
@@ -2,10 +2,11 @@ import "dotenv/config";
 
 export const ENV_VARS = {
   PORT: process.env.PORT || 5000,
-  CLIENT_URL: process.env.CLIENT_URL || "http://localhost:5173",
+  CLIENT_URL: process.env.CLIENT_URL || "https://retailshopfrontend.onrender.com",
   MONGO_URI: process.env.MONGO_URI,
   NODE_ENV: process.env.NODE_ENV,
   ACEESS_JWT_SECRET: process.env.ACEESS_JWT_SECRET,
   REFRESH_JWT_SECRET: process.env.REFRESH_JWT_SECRET,
   VITE_API_URL: process.env.VITE_API_URL || "https://retailshopbackend.onrender.com"
+
 };
