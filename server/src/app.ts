9a617cede865767a9a0b3180580504f00255b322
eddolody-ui <eddolody@gmail.com>
2026-01-07 09:51:54 +0630

    add02

--- server/src/app.ts
+++ server/src/app.ts
@@ -36,6 +36,7 @@ app
           "http://localhost:5173",
           "http://localhost:5174",
           "http://localhost:5175",
+          "https://retailshopfrontend.onrender.com",
         ];
         if (!origin) return callback(null, true);
         if (whiteList.includes(origin)) {
